2296f777717d254d01b0438bb11c5b12c8bbffde
root <jyosthna1>
2024-01-10 15:35:07 +0000

    new 10 changes1

--- src/components/BookItem/index.js
+++ src/components/BookItem/index.js
@@ -1,14 +1,17 @@
+import {Link} from 'react-router-dom'
 import './index.css'
 
 const BookItem = props => {
   const {details} = props
-  const {title, coverPic, authorName} = details
+  const {title, coverPic, authorName, id} = details
   return (
-    <div className="book-container">
-      <img src={coverPic} className="BookImage" alt={title} />
-      <h1 className="titleOfBook">{title}</h1>
-      <h1 className="authorNameOfBook">{authorName}</h1>
-    </div>
+    <Link to={`/books/${id}`} className="link-style-item-home">
+      <div className="book-container">
+        <img src={coverPic} className="BookImage" alt={title} />
+        <h1 className="titleOfBook">{title}</h1>
+        <h1 className="authorNameOfBook">{authorName}</h1>
+      </div>
+    </Link>
   )
 }
 
