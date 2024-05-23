b42a461a01075f9b8675a1faac5eab3d135acf5e
jyosthna1 <103921484+jyosthna1@users.noreply.github.com>
2024-05-23 20:22:17 +0530

    Update index.js

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -51,50 +51,13 @@ class Login extends Component {
 
     const {username, password, showSubmitError, errorMsg} = this.state
     return (
-      <form className="login-form" onSubmit={this.onSubmitUserDetails}>
+      <div className="login-form">
         <img
           src="https://res.cloudinary.com/dhcm3a6yw/image/upload/v1703999381/Rectangle_1467_sgtzoo.png"
           alt="login website logo"
           className="loginWebsiteLogo"
         />
-        <div className="loginDetailsContainer">
-          <div className="login-details">
-            <img
-              src="https://res.cloudinary.com/dhcm3a6yw/image/upload/v1704001373/Group_7732_fknwex.png"
-              alt="website login"
-              className="websiteLogin"
-            />
-            <div className="username-container">
-              <label htmlFor="username" className="usernameLabel">
-                username*
-              </label>
-              <input
-                type="text"
-                className="userInput"
-                id="username"
-                onChange={this.onChangeUsername}
-                value={username}
-              />
-            </div>
-            <div className="password-container">
-              <label htmlFor="password" className="usernameLabel">
-                Password*
-              </label>
-              <input
-                type="password"
-                className="userInput"
-                id="password"
-                onChange={this.onChangePassword}
-                value={password}
-              />
-            </div>
-            {showSubmitError && <p className="errorMsg">{errorMsg}</p>}
-            <button type="submit" className="login-button">
-              Login
-            </button>
-          </div>
-        </div>
-      </form>
+      </div>
     )
   }
 }
